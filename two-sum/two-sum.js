8e6d780c52e9a58198e08bacbf9baf9cef9eba71
Sven Hafemeister <sven@paratech.ca>
2019-12-09 14:22:25 -0800

    added array comparison function

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -17,6 +17,16 @@ var assertEqualArray = function (item1, item2) {
         throw "Failed - one of the items is not an array."
     }
 
+    if (item1.length !== item2.length) {
+        throw "Failed - array sizes do not match."
+    }
+
+    for (let i = 0; i < item1.length; i++) {
+        if (item1[i] !== item2[i]) {
+            throw "Failed - array items not the same"
+        }
+    }
+
     console.log("Passed", item1, item2);
 }
 
